13163ee2d6f89729a6bab38de0cd1288b65f014d
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-04-02 18:05:17 -0300

    Starting building the Movie Screen

--- navigation.js
+++ navigation.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import HomeScreen from "./screens/HomeScreen"
+import MovieScreen from "./screens/MovieScreen"
 
 const Stack = createNativeStackNavigator()
 
@@ -9,6 +10,7 @@ function Navigation() {
 		<NavigationContainer>
 			<Stack.Navigator screenOptions={{ headerShown: false }}>
 				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Movie" component={MovieScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	)
